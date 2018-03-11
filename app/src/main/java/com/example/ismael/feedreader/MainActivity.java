d2e23bb34ee16254d8cda59981ce022697d1f87c
Ismael Núñez <ismael.inc@gmail.com>
2018-03-11 19:37:00 +0100

    rreordenados feeds

--- app/src/main/java/com/example/ismael/feedreader/MainActivity.java
+++ app/src/main/java/com/example/ismael/feedreader/MainActivity.java
@@ -36,19 +36,21 @@ public class MainActivity extends AppCompatActivity {
     /* TODO Añadir aquí los podcasts con nombre y enlace */
     public static final ElementoSpinner[] fuentes = {
             new ElementoSpinner("GeekyTheory", "https://geekytheory.com/feed"),
+            new ElementoSpinner("Novoda", "https://blog.novoda.com/rss/"),
             new ElementoSpinner("Palabra de hacker", "http://www.ivoox.com/palabra-hacker_fg_f1266057_filtro_1.xml"),
-            new ElementoSpinner("Play Rugby", "http://cadenaser.com/programa/rss/play_rugby/"),
-            new ElementoSpinner("Oh My LOL", "https://recursosweb.prisaradio.com/podcasts/571.xml"),
-            new ElementoSpinner("El transistor", "http://www.ondacero.es/rss/podcast/644375/podcast.xml"),
-            new ElementoSpinner("Canciones Orlando", "http://practicascursodam.esy.es/musica/milista.m3u"),
+
             new ElementoSpinner("EthereumWorldNews", "https://ethereumworldnews.com/feed/"),
             new ElementoSpinner("CryptoVest", "https://cryptovest.com/feed/"),
-            new ElementoSpinner("Cointelegraph", "https://cointelegraph.com/rss"),
             new ElementoSpinner("InvestInBlockchain", "https://www.investinblockchain.com/category/news/feed/"),
             new ElementoSpinner("Bitfalls", "https://bitfalls.com/feed/"),
             new ElementoSpinner("Cryptoninjas", "https://www.cryptoninjas.net/feed/"),
-            //new ElementoSpinner("CrypotPanic", "https://cryptopanic.com/about/api/"), NO furulA
-            new ElementoSpinner("InsideBitcoins", "https://insidebitcoins.com/feed")
+            //new ElementoSpinner("Cointelegraph", "https://cointelegraph.com/rss"),
+            //new ElementoSpinner("InsideBitcoins", "https://insidebitcoins.com/feed"),
+
+            new ElementoSpinner("Play Rugby", "http://cadenaser.com/programa/rss/play_rugby/"),
+            new ElementoSpinner("Oh My LOL", "https://recursosweb.prisaradio.com/podcasts/571.xml"),
+            new ElementoSpinner("El transistor", "http://www.ondacero.es/rss/podcast/644375/podcast.xml"),
+            new ElementoSpinner("Canciones Orlando", "http://practicascursodam.esy.es/musica/milista.m3u")
     };
 
     private ListView lista;
